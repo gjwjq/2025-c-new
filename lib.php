17b885c1c0015862e6606bd1d1fbda90ffb13f88
gjwjq <jaykwon615@gmail.com>
2026-02-05 11:51:56 +0900

    Router, lib

---
+++ lib.php
@@ -0,0 +1,28 @@
+<?php
+
+function ss() {
+  return $_SESSION['ss'] ?? false;
+}
+
+function script($s) {
+  echo "<script>$s</script>";
+}
+function alert($a) {
+  script("alert($a)");
+}
+function move($m, $a = null) {
+  if($a) alert($a);
+  script("location.href = '$m'");
+}
+
+function back($a = null) {
+  if($a) alert($a);
+  script("history.back()");
+}
+
+function views($page, $data = [], $n = true) {
+  extract($data);
+    require_once "./views/templates/header.php";
+    require_once "./views/$page.php";
+    require_once "./views/templates/footer.php";
+}
